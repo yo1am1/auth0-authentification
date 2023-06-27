5127e541db5d62807030e70bb99a5deb4cbb4ac5
yo1am1 <bigdiebam@gmail.com>
2023-06-28 02:57:35 +0300

    .

--- webappexample/settings.py
+++ webappexample/settings.py
@@ -84,12 +84,7 @@ WSGI_APPLICATION = "webappexample.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
